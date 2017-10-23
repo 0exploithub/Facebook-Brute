e2975e9ab3ad9977507e47e4eba4792d21a27d87
ExploitHub <33017598+0exploithub@users.noreply.github.com>
2017-10-23 12:25:45 +0530

    Add files via upload

---
+++ package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt-get install gcc -y
+
+apt-get install python -y
+
+apt-get install Build-essential -y
+
+apt-get install perl -y
+
+apt-get install git -y
+
+apt-get install libany-uri-escape-perl -y
+
+apt-get install libhtml-html5-entities-perl -y
+
+apt-get install libhtml-entities-numbered-perl -y
+
+apt-get install libhtml-parser-perl -y
+
+apt-get install libwww-perl -y
